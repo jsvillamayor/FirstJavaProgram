79f98ee44a3c1c57470c317079db1ffbf103e6af
jsvillamayor <78101377+jsvillamayor@users.noreply.github.com>
2022-03-29 12:06:57 -0700

    Add files via upload

---
+++ myFirstJavaProgram/src/myFirstJavaProgram/Main.java
@@ -0,0 +1,13 @@
+package myFirstJavaProgram;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("I love pizza");
+		System.out.print("It's really good\n");
+		System.out.print("My favorite is cheese pizza.");
+
+	}
+
+}
